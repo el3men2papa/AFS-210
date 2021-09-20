43ebd408990cea594d9b1924f4c905854f3014d0
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2021-09-19 23:56:46 -0400

    first commit

--- week6/Shuffle/shuffle.py
+++ week6/Shuffle/shuffle.py
@@ -1,31 +1,31 @@
 import random
 
-def myShuffle(nlist):
+def myShuffle(jlist):
     
-    # Time Complexity of this Algorithm: O(n) - Linear Time
     
-    list_length = len(nlist)
+    list_length = len(jlist)
     
     for i in range(list_length):
         
-        # generate a random number based on the size of the list
+        # generate a random number 
         random_num = random.randint(0, list_length - 1)
 
-        # pop off that random element and append to the end of the list
-        list_element = nlist.pop(random_num)
-        nlist.append(list_element)
+        # pop off that random element and add it to the end of the list
+        list_element = jlist.pop(random_num)
+        jlist.append(list_element)
         
         # pop off the middle number and append it to the end of the list
         mid_point = list_length // 2
-        mid_number = nlist.pop(mid_point)
-        nlist.append(mid_number)
+        mid_number = jlist.pop(mid_point)
+        jlist.append(mid_number)
 
         # pop off the first number and append it to the end of the list
-        first_number = nlist.pop(0)
-        nlist.append(first_number)
+        first_number = jlist.pop(0)
+        jlist.append(first_number)
     
-    return nlist
+    return jlist
 
+#list of numbert inputed
 myList = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 
 print('\nBefore Shuffle:\n')
--- week7/Media_Player/mediaplayer.py
+++ week7/Media_Player/mediaplayer.py
@@ -1,4 +1,9 @@
-class Song:
+from queue import Queue
+import random
+
+# Song Class
+#region
+class Song():
     def __init__(self,title,artist):
         self.title = title
         self.artist = artist
@@ -23,10 +28,114 @@ class Song:
         
     def __gt__(self, other):
         return ((self.title, self.artist) < (other.title, other.artist))
+#endregion
+
+# Queue Class
+#region
+class Queue:
+    def __init__(self, current_song_index=0, currently_playing=False):
+        self.items = []
+        self.set_current_song_index(current_song_index)
+        self.set_currently_playing(currently_playing)
+
+    def set_current_song_index(self, index):
+        self.current_song_index = index
+
+    def set_currently_playing(self, currently_playing):
+        self.curently_playing = currently_playing
+
+    def add_song(self, song):
+        if self.size() > 0:
+            self.items.insert(self.size() + 1, song)
+        else:
+            self.items.insert(0, song)
+
+    def size(self):
+        return len(self.items)
+
+    def play(self, song_index):
+
+        self.current_song_index = song_index
         
+        self.set_currently_playing(True)
+        
+        print("\nPlaying....")
 
+        print(self.items[self.current_song_index])
 
+    def show_play_list(self):
+        num = 1
+
+        print("\nSong list:\n")
+
+        for item in self.items:
+            print(str(num) + '. ' + str(item))
+            num += 1
+
+    def show_current_song(self):
+        print("\nCurrently playing:")
+
+        print(self.items[self.current_song_index])
+
+    def next(self):
+        
+        print('Currently Playing: ', self.curently_playing)
+        
+        playlist = len(self.items) - 1
+
+        if self.current_song_index == playlist:
+            next_song = 0
+        else:
+            next_song = self.current_song_index + 1
+
+        print("\nSkipping....")
+
+        self.play(next_song)
+
+    def prev(self):
+        playlist = len(self.items) - 1
+
+        if self.current_song_index == playlist:
+            prev_song = 0
+        else:
+            prev_song = self.current_song_index - 1
+
+        print("\nReplaying....")
+
+        self.play(prev_song)
+
+    def removeSong(self, title):
+        song_index = 0
+
+        for item in self.items:
+            if item.title == title:
+                break   
+            song_index += 1
+
+        self.items.pop(song_index)
+        
+    def shuffle(self):
+        list_length = self.size()
+        
+        for i in range(list_length):
+            
+            # pop off the first element and append it to the end of the list
+            first_number = self.items.pop(0)
+            self.items.append(first_number)
+            
+            # generate a random number based on the size of the list
+            random_num = random.randint(0, list_length - 1)
+
+            # pop off that random element and append to the end of the list
+            list_element = self.items.pop(random_num)
+            self.items.append(list_element)
+
+#endregion
+
+# Menu 
+#region 
 def menu():
+    print('\n')
     print(20 * "-" , "MENU" , 20 * "-")
     print("1. Add Song to Playlist")
     print("2. Remove song from Playlist")
@@ -38,42 +147,90 @@ def menu():
     print("8. Show Current Playlist Order")
     print("0. Exit")
     print(47 * "-")
+    print('\n')
+#endregion
+    
+media_player = Queue()
 
+# Default Playlist
+media_player.add_song(Song('The Walk', 'Mayer Hawthorne'))
+media_player.add_song(Song('What Lovers Do', 'Maroon 5'))
+media_player.add_song(Song('Heartbreaker', 'Mariah Carey'))
+media_player.add_song(Song('24K Magic', 'Bruno Mars'))
+media_player.add_song(Song('Wifey', 'Next'))
+media_player.add_song(Song('Rockit', 'Herbie Hancock'))
+media_player.add_song(Song('Dress You Up', 'Madonna'))
+media_player.add_song(Song('Midas Touch', 'Midnight Star'))
 
+# Media Player
+#region
 while True:
     menu()
     choice = int(input())
 
     if choice == 1:
         # Add code to prompt user for Song Title and Artist Name
+        artist = input('Enter the Song Artist: ')
+        title = input('Enter Song title: ')
+
         # Add song to playlist
-        print("New Song Added to Playlist")
+        song = Song(title=title, artist=artist)
+        media_player.add_song(song)
+        
+        print("New Song: " + song.title + " Added to Playlist")
+        
     elif choice == 2:
+    
         # Prompt user for Song Title 
+        title = input('Enter the song title to be removed: ')
+        
         # remove song from playlist
-        print("Song Removed to Playlist")
+        media_player.removeSong(title)
+        
+        print("Song: " + title + " Removed from Playlist")
+
     elif choice == 3:
         # Play the playlist from the beginning
         # Display song name that is currently playing
-        print("Playing....")        
+
+        media_player.play(0)
+        
     elif choice == 4:
         # Skip to the next song on the playlist
         # Display song name that is now playing
-        print("Skipping....")                     
+
+        media_player.next()
+        
     elif choice == 5:
         # Go back to the previous song on the playlist
         # Display song name that is now playing
-        print("Replaying....")  
+
+        media_player.prev()
+
     elif choice == 6:
         # Randomly shuffle the playlist and play the first song
         # Display song name that is now playing
-        print("Shuffling....")          
+
+        media_player.shuffle()
+
+        print("Shuffling....")
+
+        media_player.show_play_list()
+
+        media_player.play(0)
+
     elif choice == 7:
+
         # Display the song name and artist of the currently playing song
-        print("Currently playing: ", end=" ")    
+        media_player.show_current_song()
+
     elif choice == 8:
+
         # Show the current song list order
-        print("\nSong list:\n")
+        media_player.show_play_list()
+
     elif choice == 0:
+
         print("Goodbye.")
         break
+#endregion
